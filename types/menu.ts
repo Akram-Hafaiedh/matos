6be5fb4f5644a6bb00e2d6e66ecd67bfe7593cfe
Hafaiedh Akram <hfaiedhakram@gmail.com>
2026-01-27 11:40:38 +0100

    logo, menu and images

---
+++ types/menu.ts
@@ -0,0 +1,31 @@
+export interface MenuItem {
+    id: string;
+    name: string;
+    price?: number | Record<string, number> | null;
+    ingredients?: string;
+    popular?: boolean;
+    bestseller?: boolean;
+    hot?: boolean;
+    image: string; // Emoji for now, can be replaced with real images later
+    category:
+    | 'pizza'
+    | 'burger'
+    | 'tacos'
+    | 'makloub'
+    | 'sandwich'
+    | 'plat'
+    | 'salade'
+    | 'sides'
+    | 'tunisian'
+    | 'kids'
+    | 'drinks'
+    | 'dessert'
+    | 'supplements';
+}
+
+
+export interface Category {
+    id: string;
+    name: string;
+    emoji: string;
+}
